a2dc1e0786052bd2961480ca09a0297a678531c0
Cozy Kuns <otwojoy@gmail.com>
2020-02-28 16:30:34 -0800

    git gud

--- test.js
+++ test.js
@@ -2,25 +2,54 @@ function tileValue(tile) {
   return (tile >= 27 ? 10 : (tile % 9 + 1));
 }
 
+function getAtamaFromConfiguration(configuration) {
+  let atama = undefined; 
+
+  for (let i = 0; i < configuration.length; i++) {
+    const group = configuration[i];
+
+    if (group.length === 2 && group[0] === group[1]) {
+      atama = group;
+      break;
+    } else if (group.length === 1) {
+      atama = group;
+    }
+  }
+
+  return atama;
+}
+
+function calcConfigurationShanten(configuration) {
+  const atama = getAtamaFromConfiguration(configuration);
+
+  const configurationWithoutAtama = configuration.slice();
+  configurationWithoutAtama.splice(configuration.indexOf(atama), 1);
+
+  const groupsToUse = configurationWithoutAtama.sort(
+    (a, b) => b.length - a.length
+  ).slice(0, 4);
+
+  return 9 - atama.length - groupsToUse.reduce((a, b) => a + b.length - 1, 0);
+}
+
 function calcMentsuConfigurations(hand) {
   const configurations = [];
-  let queue = [[hand, []]];
+  let queue = [[hand, [], false]];
 
   while (queue.length > 0) {
     const nextElement = queue.shift();
 
     const currentHand = nextElement[0];
     const oldHand = nextElement[1];
+    const hasAtama = nextElement[2];
 
     if (currentHand.length === 0) {
       // The hand needs to have a head candidate to be considered a valid 
       // configuration.
-      if (
-        oldHand.some(group => group.length === 1) || 
-        oldHand.some(group => group.length === 2 && group[0] === group[1])
-      ) {
+      if (hasAtama) {
         // Remove all elements that do not have an empty hand from the queue.
         queue = queue.filter(element => element[0].length === 0);
+
         configurations.push(oldHand);
       }
 
@@ -33,6 +62,7 @@ function calcMentsuConfigurations(hand) {
         queue.push([
           currentHand.slice(3, currentHand.length),
           oldHand.concat([currentHand.slice(0, 3)]),
+          hasAtama,
         ]);
       }
 
@@ -47,7 +77,7 @@ function calcMentsuConfigurations(hand) {
           newHand.splice(newHand.indexOf(currentHand[0] + 2), 1);
 
           const shuntsu = [currentHand[0], currentHand[0] + 1, currentHand[0] + 2];
-          queue.push([newHand, oldHand.concat([shuntsu])]);
+          queue.push([newHand, oldHand.concat([shuntsu]), hasAtama]);
         }
 
       }
@@ -59,6 +89,7 @@ function calcMentsuConfigurations(hand) {
         queue.push([
           currentHand.slice(2, currentHand.length),
           oldHand.concat([currentHand.slice(0, 2)]),
+          true,
         ]);
       }
 
@@ -68,7 +99,7 @@ function calcMentsuConfigurations(hand) {
         newHand.splice(newHand.indexOf(currentHand[0] + 1), 1);
 
         const taatsu = [currentHand[0], currentHand[0] + 1];
-        queue.push([newHand, oldHand.concat([taatsu])]);
+        queue.push([newHand, oldHand.concat([taatsu]), hasAtama]);
       }
 
       // Kanchan
@@ -77,7 +108,7 @@ function calcMentsuConfigurations(hand) {
         newHand.splice(newHand.indexOf(currentHand[0] + 2), 1);
 
         const taatsu = [currentHand[0], currentHand[0] + 2];
-        queue.push([newHand, oldHand.concat([taatsu])]);
+        queue.push([newHand, oldHand.concat([taatsu]), hasAtama]);
       }
     }
 
@@ -85,6 +116,7 @@ function calcMentsuConfigurations(hand) {
     queue.push([
       currentHand.slice(1, currentHand.length),
       oldHand.concat([currentHand.slice(0, 1)]),
+      true,
     ]);
   }
 
@@ -93,9 +125,15 @@ function calcMentsuConfigurations(hand) {
 
 let hand = [1, 2, 6, 7, 10, 11, 15, 16, 19, 20, 24, 25, 26];
 
-const hrStart = process.hrtime()
+const hrStart = process.hrtime();
+
 for (let i = 0; i < 1000; i++) {
-  calcMentsuConfigurations(hand);
+  const configurations = calcMentsuConfigurations(hand);
+
+  configurations.forEach(configuration => {
+    calcConfigurationShanten(configuration);
+  });
 }
+
 const hrEnd = process.hrtime(hrStart);
 console.log(hrEnd[0], hrEnd[1] / 1000000);
